c97635e8ce13f42115e50947c6e9896a8d4a373a
brinkqiang <brink.qiang@gmail.com>
2018-09-12 14:45:00 +0800

    add

--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,7 @@
 #include "dmlog.h"
-
+#include "dmtime.h"
 int main(int argc, char *argv[]) {
-  CDMLog::Instance()->DMLogSetPropFilename("demo.log");
+
   LOG_INFO("This is a basic logging message.");
 
   LOG_INFO("This message includes {} substitutions, similar to how {} work.", 2, 
@@ -18,14 +18,17 @@ int main(int argc, char *argv[]) {
   LOG_DEBUG("This is a debugging message with blue coloring.");
   LOG_TRACE("This is a tracing message with cyan coloring.");
 
-
+  CDMLog::Instance()->DMLogSetPropFilename("demo.log");
   LOG_INFO("This will be printed to the console, as well as \"demo.log\" as its first entry.");
 
   CDMLog::Instance()->DMLogSetPropStream(DMLOG_STREAM_DISK);
-  CDMLog::Instance()->DMLogSetPropRollover(2048);
-  for(int i = 0; i < 15; i++) {
-    LOG_INFO("This message is 116 characters long including the info that comes before it.");
+  CDMLog::Instance()->DMLogSetPropRollover(LONG_MAX);
+  LOG_INFO("start time: {:%Y-%m-%d %H:%M:%S}", fmt::localtime(time(NULL)));
+  for(int i = 0; i < 1000000; i++) {
+    LOG_INFO("This message is 116 characters long including the info that comes before it. {}", i);
   }
+  LOG_INFO("end time: {:%Y-%m-%d %H:%M:%S}", fmt::localtime(time(NULL)));
+
   LOG_INFO("This message will cause the file to exceed the rollover value, which triggers it to "
            "be closed and renamed with an incremental number appended to it. A new file with the "
            "original filename is then opened and used in its place.");
