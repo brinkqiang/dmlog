314bb0b1d4f9a709bf491d0af64836054b2ff375
brinkqiang <brink.qiang@gmail.com>
2020-12-26 19:05:19 +0800

    --wip-- [skip ci]

--- build.bat
+++ build.bat
@@ -7,7 +7,7 @@ rem git submodule update --init --recursive
 rmdir /S /Q build
 mkdir build
 pushd build
-cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+cmake -A win32 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
 cmake --build . --config relwithdebinfo
 popd
 
