6747b24a373d2c64b90c1adb0e256ed5089864f1
brinkqiang <brink.qiang@gmail.com>
2022-08-31 11:25:08 +0800

    add

--- test/dmlogtest/main.cpp
+++ test/dmlogtest/main.cpp
@@ -1,10 +1,10 @@
 #include "dmlog.h"
 #include "dmutil.h"
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     DMBench oBench;
     LOG_CRITICAL("hello world {}.", 2019);
-
+    LOG_CRITICAL("It is {:%Y-%m-%d %H:%M:%S}\n", fmt::localtime(time(NULL)));
     for (int i = 0; i < 2000000; i++) {
         oBench++;
         LOG_INFO("This message is 116 characters long including the info that comes before it. {}", i);
